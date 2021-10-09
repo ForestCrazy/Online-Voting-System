71f18d72069530acdabd31c47e08d77ac555917f
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-10-09 19:00:28 +0700

    add isAdmin function

--- _system/oop.php
+++ _system/oop.php
@@ -1,8 +1,30 @@
 <?php
-function gotoPage($page) {
-    ?>
+function gotoPage($page)
+{
+?>
     <script>
         window.location.href = '<?= $page ?>';
     </script>
-    <?php
+<?php
+}
+
+function isAdmin($u_id)
+{
+    global $connect;
+    $sql_account = 'SELECT role FROM account WHERE u_id = "' . $u_id . '"';
+    $res_account = mysqli_query($connect, $sql_account);
+    if ($res_account) {
+        if (mysqli_num_rows($res_account) == 1) {
+            $fetch_account = mysqli_fetch_assoc($res_account);
+            if ($fetch_account['role'] == 'admin') {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
 }
