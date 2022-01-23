a19368033bf5151e37a4b58faae37ea62fe1a217
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2022-01-23 20:49:17 +0700

    API - election_list
    * convert str to date before checking the condition
    * add WHERE clause in sql query election  list

--- API/election_list.php
+++ API/election_list.php
@@ -2,11 +2,11 @@
 require_once("../_system/config.php");
 require_once("../_system/database.php");
 $datenow = date("Y-m-d H:i:s");
-$sql_votelist = 'SELECT * FROM election';
+$sql_votelist = 'SELECT * FROM election WHERE hidden_time >= NOW()';
 if (isset($_GET["keyword"])) {
     $keyword = mysqli_real_escape_string($connect, $_GET["keyword"]);
     if (!$_GET["keyword"] == "" || !$_GET["keyword"] == "NULL" || !$_GET["keyword"] == NULL) {
-        $sql_votelist = 'SELECT * FROM election WHERE election_id LIKE "%'. $keyword .'%" OR title LIKE "%'. $keyword .'%" OR description = "%'. $keyword .'%"';
+        $sql_votelist = 'SELECT * FROM election WHERE hidden_time >= NOW() AND (election_id LIKE "%'. $keyword .'%" OR title LIKE "%'. $keyword .'%" OR description = "%'. $keyword .'%")';
     }
 } else {
     
@@ -55,12 +55,12 @@ $resElectionList = mysqli_query($connect, $sql_votelist);
 $electionArray = array();
 while($fetchElection = mysqli_fetch_assoc($resElectionList)) {
     $fetchElection["election_state"] = 1;
-    $dateNow = new DateTime(date("Y-m-d H:i:s"));
-    if ($fetchElection["announcement_time"] <= $dateNow) {
+    $dateNow = date("Y-m-d H:i:s");
+    if (date("Y-m-d H:i:s", strtotime($fetchElection["announcement_time"])) <= $dateNow) {
         $fetchElection["election_state"] = 4;
-    } else if ($fetchElection["end_time"] <= $dateNow) {
+    } else if (date("Y-m-d H:i:s", strtotime($fetchElection["end_time"])) <= $dateNow) {
         $fetchElection["election_state"] = 3;
-    } else if ($fetchElection["start_time"] <= $dateNow) {
+    } else if (date("Y-m-d H:i:s", strtotime($fetchElection["start_time"])) <= $dateNow) {
         $fetchElection["election_state"] = 2;
     }
     array_push($electionArray, $fetchElection);
