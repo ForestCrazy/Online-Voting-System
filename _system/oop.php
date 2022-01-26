abd6dbc1c1c4fe9fe65ca3dc0613395e1425ddae
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2022-01-26 23:54:15 +0700

    SYS - add DateThai function

--- _system/oop.php
+++ _system/oop.php
@@ -28,3 +28,16 @@ function isAdmin($u_id)
         return false;
     }
 }
+
+function DateThai($strDate)
+{
+    $strYear = date("Y", strtotime($strDate)) + 543;
+    $strMonth = date("n", strtotime($strDate));
+    $strDay = date("j", strtotime($strDate));
+    $strHour = date("H", strtotime($strDate));
+    $strMinute = date("i", strtotime($strDate));
+    $strSeconds = date("s", strtotime($strDate));
+    $strMonthCut = array("", "ม.ค.", "ก.พ.", "มี.ค.", "เม.ย.", "พ.ค.", "มิ.ย.", "ก.ค.", "ส.ค.", "ก.ย.", "ต.ค.", "พ.ย.", "ธ.ค.");
+    $strMonthThai = $strMonthCut[$strMonth];
+    return "$strDay $strMonthThai $strYear, $strHour:$strMinute";
+}
