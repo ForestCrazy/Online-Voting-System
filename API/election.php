78e9134adcc3c66fc01f98398f5964ebe423f73f
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2022-01-27 00:37:21 +0700

    API - election_state
    * add function ```electionState```

--- API/election.php
+++ API/election.php
@@ -1,7 +1,19 @@
 <?php
 require_once("../_system/config.php");
 require_once("../_system/database.php");
-$datenow = date("Y-m-d H:i:s");
+function electionState($election)
+{
+    $electionState = 1;
+    $dateNow = date("Y-m-d H:i:s");
+    if (date("Y-m-d H:i:s", strtotime($election["announcement_time"])) <= $dateNow) {
+        $electionState = 4;
+    } else if (date("Y-m-d H:i:s", strtotime($election["end_time"])) <= $dateNow) {
+        $electionState = 3;
+    } else if (date("Y-m-d H:i:s", strtotime($election["start_time"])) <= $dateNow) {
+        $electionState = 2;
+    }
+    return $electionState;
+}
 $sql_votelist = 'SELECT * FROM election WHERE hidden_time >= NOW()';
 if (isset($_GET["keyword"])) {
     $keyword = mysqli_real_escape_string($connect, $_GET["keyword"]);
@@ -18,19 +30,13 @@ if (isset($_GET["keyword"])) {
 }
 $resElectionList = mysqli_query($connect, $sql_votelist);
 if ($specificElectionSearch) {
-    echo json_encode(mysqli_fetch_assoc($resElectionList));
+    $fetchElection = mysqli_fetch_assoc($resElectionList);
+    $fetchElection["election_state"] = electionState($fetchElection);
+    echo json_encode($fetchElection);
 } else {
     $electionArray = array();
     while ($fetchElection = mysqli_fetch_assoc($resElectionList)) {
-        $fetchElection["election_state"] = 1;
-        $dateNow = date("Y-m-d H:i:s");
-        if (date("Y-m-d H:i:s", strtotime($fetchElection["announcement_time"])) <= $dateNow) {
-            $fetchElection["election_state"] = 4;
-        } else if (date("Y-m-d H:i:s", strtotime($fetchElection["end_time"])) <= $dateNow) {
-            $fetchElection["election_state"] = 3;
-        } else if (date("Y-m-d H:i:s", strtotime($fetchElection["start_time"])) <= $dateNow) {
-            $fetchElection["election_state"] = 2;
-        }
+        $fetchElection["election_state"] = electionState($fetchElection);
         array_push($electionArray, $fetchElection);
     }
     echo json_encode($electionArray);
