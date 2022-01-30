6adf68c9bf6544102fe7fb7a8c7e77a2b3cdfa98
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2022-01-30 12:18:38 +0700

    DB - change username to u_id

--- sql/votelog.sql
+++ sql/votelog.sql
@@ -6,7 +6,7 @@ SET time_zone = "+07:00";
 CREATE TABLE IF NOT EXISTS `votelog` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `election_id` int(5) ZEROFILL NOT NULL,
-  `username` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `u_id` int(10) NOT NULL,
   `vote_time` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
