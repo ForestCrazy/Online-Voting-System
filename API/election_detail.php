4fade66d7f2d44ece645b5f8a1bdd06b413dc35d
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2022-02-08 11:50:59 +0700

    API - handle params missing

--- API/election_detail.php
+++ API/election_detail.php
@@ -2,14 +2,16 @@
 require_once("../_system/config.php");
 require_once("../_system/database.php");
 $datenow = date("Y-m-d H:i:s");
-if (isset($_GET["keyword"])) {
-    $keyword = mysqli_real_escape_string($connect, $_GET["keyword"]);
-        $sql_votelist = 'SELECT start_time, end_time, announcement_time FROM election WHERE election_id = "'. $keyword .'"';
+if (!isset($_GET["keyword"])) {
+    $keyword = '';
+}
+$keyword = mysqli_real_escape_string($connect, $_GET["keyword"]);
+$sql_votelist = 'SELECT start_time, end_time, announcement_time FROM election WHERE election_id = "' . $keyword . '"';
 $res_votelist = mysqli_query($connect, $sql_votelist);
 $num_votelist = mysqli_num_rows($res_votelist);
 
 $resultArray = array();
-while($result = mysqli_fetch_array($res_votelist,MYSQLI_ASSOC)){
+while ($result = mysqli_fetch_array($res_votelist, MYSQLI_ASSOC)) {
     $datetime1 = new DateTime(date("Y-m-d H:i:s"));
     if ($result["announcement_time"] <= $datenow) {
         $html = "1";
@@ -31,13 +33,9 @@ while($result = mysqli_fetch_array($res_votelist,MYSQLI_ASSOC)){
         $result["format_date"] = $format_date;
         $result["html"] = $html;
     }
-    array_push($resultArray,$result);
+    array_push($resultArray, $result);
     //print_r($result);
 }
 
 echo json_encode($resultArray);
-//print_r($resultArray);
-
-} else {
-    echo 'Error: =w=';
-}
+    //print_r($resultArray);
