5b67fd78edca1745b2853f811d954c2c4be021db
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2022-02-15 18:25:09 +0700

    DB - add pre_fix field

--- sql/account.sql
+++ sql/account.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS `account` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `username` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `password` varchar(600) NOT NULL,
+  `pre_fix` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `FirstName` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci,
   `LastName` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci,
   `role` ENUM('admin', 'user') DEFAULT 'user',
