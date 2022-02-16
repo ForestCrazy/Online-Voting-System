d5dd39f7f1b7e6f79445e3bf75244ec86f71bd24
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2022-02-16 21:25:51 +0700

    API - add update pre_fix field

--- API/admin/edit_account.php
+++ API/admin/edit_account.php
@@ -4,7 +4,7 @@ require '../../_system/database.php';
 require '../../_system/oop.php';
 if (isset($_SESSION['u_id'])) {
     if (isAdmin($_SESSION['u_id'])) {
-        $sql_edit_account = 'UPDATE account SET FirstName = "' . $_POST['FirstName'] . '", LastName = "' . $_POST['LastName'] . '", role = "' . $_POST['role'] . '" WHERE id = "' . $_POST['id'] . '"';
+        $sql_edit_account = 'UPDATE account SET pre_fix = "' . $_POST['pre_fix'] . '", FirstName = "' . $_POST['FirstName'] . '", LastName = "' . $_POST['LastName'] . '", role = "' . $_POST['role'] . '" WHERE id = "' . $_POST['id'] . '"';
         $res_edit_account = mysqli_query($connect, $sql_edit_account);
         if ($res_edit_account) {
             echo json_encode(array("success" => true));
