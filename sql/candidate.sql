9effbf3e4495d363f9ad162530a45371f79fe222
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2022-02-16 20:55:50 +0700

    DB - add u_id field

--- sql/candidate.sql
+++ sql/candidate.sql
@@ -5,6 +5,7 @@ SET time_zone = "+07:00";
 CREATE TABLE IF NOT EXISTS `candidate` (
   `election_id` int(5) ZEROFILL NOT NULL,
   `cdd_id` int(5) NOT NULL,
+  `u_id` int(10) NOT NULL,
   `pre_fix` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `FirstName` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci,
   `LastName` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci,
