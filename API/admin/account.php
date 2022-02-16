f771893d15df25b84ac8ca8f263b425be336092b
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2022-02-16 20:57:42 +0700

    API - add username parameter

--- API/admin/account.php
+++ API/admin/account.php
@@ -4,14 +4,16 @@ require '../../_system/database.php';
 require '../../_system/oop.php';
 if (isset($_SESSION['u_id'])) {
     if (isAdmin($_SESSION['u_id'])) {
-        $sql_account_list = 'SELECT id, FirstName, LastName, username, role FROM account';
+        $sql_account_list = 'SELECT id, prefix, FirstName, LastName, username, role FROM account';
         if (isset($_GET['id'])) {
             $sql_account_list .= ' WHERE id = "' . $_GET['id'] . '"';
+        } else if (isset($_GET['username'])) {
+            $sql_account_list .= ' WHERE username = "' . $_GET['username'] . '"';
         }
         $res_account_list = mysqli_query($connect, $sql_account_list);
         if ($res_account_list) {
             // print_r(mysqli_fetch_all($res_account_list));
-            if (isset($_GET['id'])) {
+            if (isset($_GET['id']) || isset($_GET['username'])) {
                 echo json_encode(array("success" => true, "account" => mysqli_fetch_assoc($res_account_list)));
             } else {
                 echo json_encode(array("success" => true, "accountList" => mysqli_fetch_all($res_account_list, MYSQLI_ASSOC)));
